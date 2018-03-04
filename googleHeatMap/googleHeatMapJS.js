2ce35ca382f05ff87e95923d97bf3049788b0c4c
alecs.konson <alecs429@gmail.com>
2018-03-04 12:41:38 -0500

    Adding google point map demo

--- googleHeatMap/googleHeatMapJS.js
+++ googleHeatMap/googleHeatMapJS.js
@@ -1,6 +1,6 @@
 // This example requires the Visualization library. Include the libraries=visualization
 // parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=visualization">
+//<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8J9stgjGGM1crOqt7hQTHrdsFH3uOaho&libraries=visualization">
 
 var map, heatmap;
 
